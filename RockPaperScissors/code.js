ed8310fa2d157d884d174afc3dcc45b922cc2638
Elisa <E.Petit.inf21@stud.bbbaden.ch>
2022-10-25 10:34:51 +0200

    added switch

--- RockPaperScissors/code.js
+++ RockPaperScissors/code.js
@@ -1,1 +1,13 @@
-Math.floor(Math.random() * 10) + 1;
+switch(Math.floor(Math.random() * 3) + 1) {
+    case 1:
+      const rock = 'rock';
+      break;
+    case 2:
+        const paper = 'paper';
+      break;
+      case 3:
+        const scissor = 'scissor';
+        break;
+    default:
+      console.log("ERROR");
+  }
