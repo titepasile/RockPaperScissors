e223b5b5843d33f2f93b4fc3bee0831751c45784
Elisa <E.Petit.inf21@stud.bbbaden.ch>
2022-10-25 11:35:38 +0200

    added userinput() and switch

--- RockPaperScissors/code.js
+++ RockPaperScissors/code.js
@@ -1,18 +1,42 @@
-switch(Math.floor(Math.random() * 3) + 1) {
-    case 1:
-      const rock = 'rock';
-      break;
-    case 2:
-        const paper = 'paper';
-      break;
-      case 3:
-        const scissor = 'scissor';
-        break;
-    default:
-      console.log("ERROR");
+switch (Math.floor(Math.random() * 3) + 1) {
+  case 1:
+    const rock = 'rock'
+    break
+  case 2:
+    const paper = 'paper'
+    break
+  case 3:
+    const scissor = 'scissor'
+    break
+  default:
+    console.log('ERROR')
+}
+
+function UserInput () {
+  while (true) {
+    try {
+      var input = prompt(
+        'Its your choice: Click 1 for rock, click 2 for paper, click 3 for scissor:'
+      )
+      if (isNaN(input) || input < 1 || input > 3)
+        throw 'Your input is not valid'
+      return parseInt(input)
+    } catch (error) {
+      console.log(error)
+    }
   }
+}
 
-  console.log('Its your choise.');
-  console.lof('What do you want to choose?');
-  console.lof('rock [1], paper [2] or scissor [3]?');
-
+switch (UserInput()) {
+  case 1:
+    rock
+    break
+  case 2:
+    paper
+    break
+  case 3:
+    scissor
+    break
+  default:
+    console.log('ERROR')
+}
